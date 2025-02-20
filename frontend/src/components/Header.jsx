b65fe0b7f44cfe6705909b6bc47a27c8f5a6cde8
Rahul Yadav <arabicrahul04@gmail.com>
2025-02-20 14:54:26 +0530

    Frontend - Incomplete Header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,18 @@
+import { Logo } from "../static/images";
+
 const Header = () => {
   return (
-    <header className="bg-primary">
+    <header className="bg-dark py-4 rounded-b-3xl">
       <div className="container">
-        <h1 className="text-white">Header</h1>
+        <div className="flex items-center justify-between gap-5">
+          <img src={Logo} alt="Debate" />
+
+          <ul>
+            <li className="font-medium transition duration-500 text-light-alt hover:text-success">
+              Our Activities
+            </li>
+          </ul>
+        </div>
       </div>
     </header>
   );
